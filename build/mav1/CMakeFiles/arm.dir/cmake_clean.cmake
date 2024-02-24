dc60b877051ea3d97032e2a13ba6a6966dbdd2f1
CYMElee <leesparks8@gmail.com>
2024-02-25 03:24:01 +0800

    GUIDED(oppfoard)

---
+++ build/mav1/CMakeFiles/arm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/lee/workspace/ROS_WS/ex_qav250_ws/devel/lib/mav1/arm"
+  "/home/lee/workspace/ROS_WS/ex_qav250_ws/devel/lib/mav1/arm.pdb"
+  "CMakeFiles/arm.dir/node/arm.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/arm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/mav1/CMakeFiles/optitrack.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/lee/workspace/ROS_WS/ex_qav250_ws/devel/lib/mav1/optitrack"
+  "/home/lee/workspace/ROS_WS/ex_qav250_ws/devel/lib/mav1/optitrack.pdb"
+  "CMakeFiles/optitrack.dir/node/optitrack.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/optitrack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
